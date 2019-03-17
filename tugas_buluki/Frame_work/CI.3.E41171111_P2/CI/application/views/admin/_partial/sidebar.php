37bc8a1b8cd4ec6c30a5b944bfeac5baf35d52d2
guramin01 <48230672+guramin01@users.noreply.github.com>
2019-03-17 15:15:27 +0700

    pakvictor senin

--- tugas_buluki/Frame_work/CI.3.E41171111_P2/CI/application/views/admin/_partial/sidebar.php
+++ tugas_buluki/Frame_work/CI.3.E41171111_P2/CI/application/views/admin/_partial/sidebar.php
@@ -7,7 +7,7 @@
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           <i class="fas fa-fw fa-folder"></i>
-          <span>Pages</span>
+          <span>Master</span>
         </a>
         <div class="dropdown-menu" aria-labelledby="pagesDropdown">
           <h6 class="dropdown-header">Login Screens:</h6>
@@ -16,18 +16,33 @@
           <a class="dropdown-item" href="forgot-password.html">Forgot Password</a>
           <div class="dropdown-divider"></div>
           <h6 class="dropdown-header">Other Pages:</h6>
-          <a class="dropdown-item" href="404.html">404 Page</a>
-          <a class="dropdown-item" href="blank.html">Blank Page</a>
+          <a class="dropdown-item" href="404.html">Supplier</a>
+          <a class="dropdown-item" href="blank.html">Barang</a>
+          <a class="dropdown-item" href="blank.html">Karyawan</a>
+          <a class="dropdown-item" href="blank.html">User</a>
           <a class="dropdown-item" href="<?= base_url('index.php/admin/Overviews/test'); ?>">Dashboard2</a>
         </div>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="charts.html">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           <i class="fas fa-fw fa-chart-area"></i>
-          <span>Charts</span></a>
+          <span>Transaksi</span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+          <h6 class="dropdown-header">Transaksi :</h6>
+          <a class="dropdown-item" href="#">Penjualan</a>
+          <a class="dropdown-item" href="#">Pembelian</a>
+          
+        </div>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="tables.html">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           <i class="fas fa-fw fa-table"></i>
-          <span>Tables</span></a>
+          <span>Laporan</span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+          <h6 class="dropdown-header">Laporan :</h6>
+          <a class="dropdown-item" href="#">Laporan Harian</a>
+          <a class="dropdown-item" href="#">Laporan Bulanan</a>
+          <a class="dropdown-item" href="#">Laporan Tahunan</a>   
       </li>
