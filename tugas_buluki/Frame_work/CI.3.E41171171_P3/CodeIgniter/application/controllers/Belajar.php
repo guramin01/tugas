064717087d858de95bb3a34292c25bfdcc037d4f
athokfjrnt <45120381+athokfjrnt@users.noreply.github.com>
2019-03-24 17:48:47 +0700

    tugas lanjutan 4-8

--- tugas_buluki/Frame_work/CI.3.E41171171_P3/CodeIgniter/application/controllers/Belajar.php
+++ tugas_buluki/Frame_work/CI.3.E41171171_P3/CodeIgniter/application/controllers/Belajar.php
@@ -5,18 +5,24 @@ class Belajar extends CI_Controller {
 	
 	function __construct(){
 		parent::__construct();
-		$this->load->helper('html');
+	//	$this->load->helper('html');
+		$this->load->model('m_data');
 		
 	}
 
-	public function index(){
-		$this->load->view('view_belajar');
+	function user(){
+		$data['user'] = $this->m_data->ambil_data()->result();
+		$this->load->view('v_user.php',$data);
 	}
 
-	public function halo(){
-		 $data = array('judul' => "Cara menjadi horang kaya" ,
-		 				'isi' =>'ya kerja doong');
-		$this->load->view('view_belajar',$data);
-	}
+	//public function index(){
+	//	$this->load->view('view_belajar');
+	//}
+
+	//public function halo(){
+	//	 $data = array('judul' => "Cara menjadi horang kaya" ,
+	//	 				'isi' =>'ya kerja doong');
+	//	$this->load->view('view_belajar',$data);
+	//}
 
 }
